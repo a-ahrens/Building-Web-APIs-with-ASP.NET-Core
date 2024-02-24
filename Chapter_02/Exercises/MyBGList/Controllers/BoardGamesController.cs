c2e2a43d721081decdb38652d5ca2f65006abe67
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2024-02-24 08:17:49 -0600

    Changed folder structure to follow the path of progression of the book. Finished Exercises for Chapter 2.

---
+++ Chapter_02/Exercises/MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardGamesController : ControllerBase
+    {
+        private readonly ILogger<BoardGamesController> _logger;
+        public BoardGamesController(ILogger<BoardGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name  = "GetBoardGames")]
+        public IEnumerable<BoardGame> Get()
+        {
+            return new[]
+            {
+                new BoardGame
+                {
+                    Id = 1,
+                    Name = "Axis and Allies",
+                    Year = 1981,
+                    MinPlayers = 1,
+                    MaxPlayers = 1
+                },
+                new BoardGame
+                {
+                    Id = 2,
+                    Name = "Citadels",
+                    Year = 2000,
+                    MinPlayers = 1,
+                    MaxPlayers = 1
+                },
+                new BoardGame
+                {
+                    Id = 3,
+                    Name = "Terraforming Mars",
+                    Year = 2016,
+                    MinPlayers = 1,
+                    MaxPlayers = 1
+                }
+            };
+        }
+    }
+}
---
+++ Chapter_02/MyBGList/MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardGamesController : ControllerBase
+    {
+        private readonly ILogger<BoardGamesController> _logger;
+        public BoardGamesController(ILogger<BoardGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name  = "GetBoardGames")]
+        public IEnumerable<BoardGame> Get()
+        {
+            return new[]
+            {
+                new BoardGame
+                {
+                    Id = 1,
+                    Name = "Axis and Allies",
+                    Year = 1981
+                },
+                new BoardGame
+                {
+                    Id = 2,
+                    Name = "Citadels",
+                    Year = 2000
+                },
+                new BoardGame
+                {
+                    Id = 3,
+                    Name = "Terraforming Mars",
+                    Year = 2016
+                }
+            };
+        }
+    }
+}
