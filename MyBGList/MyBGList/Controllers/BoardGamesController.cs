47330dc82591ce38eb94c2e8bea07ad58512a810
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2024-02-27 15:49:11 -0600

    Created new DTO objects to handle data and links for Uniform Interface development. Replaced BG controller GET request with DTO version.

--- MyBGList/MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/MyBGList/Controllers/BoardGamesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBGList.DTO;
+using System.Xml.Linq;
 
 namespace MyBGList.Controllers
 {
@@ -14,27 +16,36 @@ namespace MyBGList.Controllers
 
         [HttpGet(Name  = "GetBoardGames")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)] //sets up a public cache with a max-age of 60 sec for that response
-        public IEnumerable<BoardGame> Get()
+        public RestDTO<BoardGame[]> Get()
         {
-            return new[]
+            return new RestDTO<BoardGame[]>()
             {
-                new BoardGame
+                Data = new BoardGame[]
                 {
-                    Id = 1,
-                    Name = "Axis and Allies",
-                    Year = 1981
+                    new BoardGame()
+                    {
+                        Id = 1,
+                        Name = "Axis and Allies",
+                        Year = 1981
+                    },
+
+                    new BoardGame()
+                    {
+                        Id = 2,
+                        Name = "Citadels",
+                        Year = 2000
+                    },
+
+                    new BoardGame()
+                    {
+                        Id = 3,
+                        Name = "Terraforming Mars",
+                        Year = 2016
+                    }
                 },
-                new BoardGame
+                Links = new List<LinkDTO>
                 {
-                    Id = 2,
-                    Name = "Citadels",
-                    Year = 2000
-                },
-                new BoardGame
-                {
-                    Id = 3,
-                    Name = "Terraforming Mars",
-                    Year = 2016
+                    new LinkDTO(Url.Action(null, "BoardGames", null, Request.Scheme)!, "self", "GET"),
                 }
             };
         }
