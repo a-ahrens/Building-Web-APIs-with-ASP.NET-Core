e8fd4b97e0dfd855b41820d0df1aace4eb17bf0c
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2024-02-28 14:51:05 -0600

    Created separate folder for end of chapter 3 progress. Cleaned up main project files

---
+++ Chapter_03/MyBGList_EndOfCh3/MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBGList.DTO;
+using System.Xml.Linq;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardGamesController : ControllerBase
+    {
+        private readonly ILogger<BoardGamesController> _logger;
+        public BoardGamesController(ILogger<BoardGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name  = "GetBoardGames")]
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)] //sets up a public cache with a max-age of 60 sec for that response
+        public RestDTO<BoardGame[]> Get()
+        {
+            return new RestDTO<BoardGame[]>()
+            {
+                Data = new BoardGame[]
+                {
+                    new BoardGame()
+                    {
+                        Id = 1,
+                        Name = "Axis and Allies",
+                        Year = 1981
+                    },
+
+                    new BoardGame()
+                    {
+                        Id = 2,
+                        Name = "Citadels",
+                        Year = 2000
+                    },
+
+                    new BoardGame()
+                    {
+                        Id = 3,
+                        Name = "Terraforming Mars",
+                        Year = 2016
+                    }
+                },
+                Links = new List<LinkDTO>
+                {
+                    new LinkDTO(Url.Action(null, "BoardGames", null, Request.Scheme)!, "self", "GET"),
+                }
+            };
+        }
+    }
+}
---
+++ Chapter_03/MyBGList_EndOfCh3/MyBGList/DTO/RestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBGList.DTO
+{
+    //DTO Class for hosting the actual data
+    public class RestDTO<T>
+    {
+        public List<LinkDTO> Links { get; set; } = new List<LinkDTO>();
+
+        public T Data { get; set; } = default!;
+    }
+}
